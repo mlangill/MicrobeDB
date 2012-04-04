b37ec7c6bfd343ca724bd5a49962c0766e8f1967
Morgan Langille <morgan.g.i.langille@gmail.com>
2012-04-04 12:02:11 -0300

    Manually fix schema update (since gene_accnum is already an index)

--- information/UPDATE/schema_updates/update_schema_v4.sql
+++ information/UPDATE/schema_updates/update_schema_v4.sql
@@ -3,29 +3,16 @@
 BEGIN;
 
 ALTER TABLE gene CHANGE COLUMN gene_id gene_id integer(10) unsigned NOT NULL auto_increment,
-                 ADD INDEX protein_accnum (version_id, protein_accnum),
                  ADD INDEX gene_name (gene_name(100)),
                  ADD INDEX locus_tag (locus_tag(50)),
                  ADD INDEX gene_product (gene_product(100)),
                  ENGINE=MyISAM DEFAULT CHARACTER SET latin1;
-
-ALTER TABLE genomeproject CHANGE COLUMN gpv_id gpv_id integer(10) unsigned NOT NULL auto_increment,
-                          ENGINE=MyISAM ROW_FORMAT=DYNAMIC DEFAULT CHARACTER SET latin1;
-
-ALTER TABLE microbedb_meta CHANGE COLUMN meta_id meta_id integer(10) unsigned NOT NULL auto_increment,
-                           ENGINE=MyISAM DEFAULT CHARACTER SET latin1;
 
 ALTER TABLE replicon CHANGE COLUMN rpv_id rpv_id integer(10) unsigned NOT NULL auto_increment,
                      CHANGE COLUMN file_name file_name text,
                      ADD INDEX definition (definition(100)),
                      ADD INDEX rep_ginum (rep_ginum(100)),
                      ENGINE=MyISAM DEFAULT CHARACTER SET latin1;
-
-ALTER TABLE taxonomy ENGINE=MyISAM DEFAULT CHARACTER SET latin1;
-
-ALTER TABLE version CHANGE COLUMN version_id version_id integer(11) NOT NULL auto_increment,
-                    ENGINE=MyISAM DEFAULT CHARACTER SET latin1;
-
 
 COMMIT;
 
